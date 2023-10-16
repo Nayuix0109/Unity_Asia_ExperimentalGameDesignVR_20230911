f3e899096360e8a30d2bb10dbfd797efa9ce173c
Nayuix0109 <144639481+Nayuix0109@users.noreply.github.com>
2023-10-16 11:00:55 +0800

    UpDate 新增子彈、手槍程式

---
+++ Assets/Sripts/FireSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FireSystem : MonoBehaviour
+{
+    public int magazineCount; //彈夾數量
+    public int bulletTotal; //子彈總數
+    public int bulletCurrent; //目前子彈
+}
+
+//早八不遲到不睡覺 快樂且無情的遊戲引擎人生O_Q

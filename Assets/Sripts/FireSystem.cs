6445158792e21802bf0d5c7dad5722f5c07c8cc0
Nayuix0109 <144639481+Nayuix0109@users.noreply.github.com>
2023-11-15 00:26:12 +0800

    Update 子彈飛行動畫、開槍、音效

--- Assets/Sripts/FireSystem.cs
+++ Assets/Sripts/FireSystem.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FireSystem : MonoBehaviour
 {
+    #region 資料
     [Header("彈匣的子彈數量"), Range(0, 10)]
     public int magazineCount = 7;
     [Header("子彈總數"), Range(20, 200)]
@@ -12,13 +14,23 @@ public class FireSystem : MonoBehaviour
     public GameObject prefabBullet;
     [Header("子彈生成點")]
     public Transform pointSpawnBullet;
+    [Header("開槍動畫參數")]
+    public string parFire = "觸發開槍";
+    [Header("動畫控制器")]
+    public Animator ani;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("開槍音效")]
+    public AudioClip soundFire;
+    #endregion
+
 
     //早八不遲到不睡覺 快樂且無情的遊戲引擎人生O_Q
     //public - 可讓它顯示於Unity面板
     //int = 整數
     //header為標題文字 Range在屬性面板中會顯示滑桿
 
-    public bool openDoor = true;
+    /*public bool openDoor = true;
     public bool isDead = false;
 
     private void Awake()
@@ -43,6 +55,7 @@ public class FireSystem : MonoBehaviour
             print("已經死亡");
         }
     }
+    */
 
     private void Update()
     {
@@ -52,7 +65,15 @@ public class FireSystem : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(prefabBullet);
+            ani.SetTrigger(parFire);
+            //觸發動畫控制器「觸發開槍」參數
+            Instantiate(prefabBullet,pointSpawnBullet.position, pointSpawnBullet.rotation);
+            //生成子彈在 子彈生成點位置 以及生成點角度
+
+            float volume = Random.Range(0.8f, 3.2f);
+            aud.PlayOneShot(soundFire, volume);
+            //隨機值
+
         }
     }
 }
